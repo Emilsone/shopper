16719fb24e6eb6b934ee79acb5b3fccdb982125a
Emilsone <lawsonezekiel68@gmail.com>
2021-06-08 15:49:31 -0700

    rewrite functionalities

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,31 +1,37 @@
 <template>
   <section id="shopping-app">
-<div class="shopping-app">
-          <div class="container-text">
-                <div class="text">
+    <div class="shopping-app">
+      <div class="container-text">
+        <div class="text">
+          <h1>Shopping List</h1>
 
-                    <h1>Shopping List</h1>
-                    
-                    <span><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
-                
-                </div>
-                
-            </div>
-            <div class="fruits">
-                <ul>
-                    <li v-for="(list, index) in lists" :key="index">
-                        {{ list }}
-				<i class="fa fa-trash" aria-hidden="true" v-on:click="removeList(index)"></i>
-                    </li>
-                </ul>
-            </div>
-            <div class="container">
-                <input type="text" placeholder="Create list" v-model="currentList" v-on:keyup.enter="addList" />
-                <span><i class="fa fa-plus" aria-hidden="true"></i></span>
-            </div>
-<p class="footer">Made With ❤ ❤ ❤ By Emilson</p>
-</div>
-</section>
+          <span><i class="fa fa-shopping-basket icon-font" aria-hidden="true"></i></span>
+        </div>
+      </div>
+      <div class="fruits">
+        <ul>
+          <li v-for="(list, index) in lists" :key="index">
+            {{ list }}
+            <i
+              class="fa fa-trash icon-mouse"
+              aria-hidden="true"
+              v-on:click="removeList(index)"
+            ></i>
+          </li>
+        </ul>
+      </div>
+      <div class="container">
+        <input
+          type="text"
+          placeholder="Create list"
+          v-model="currentList"
+          v-on:keyup.enter="addList"
+        />
+        <span><i class="fa fa-plus" aria-hidden="true"></i></span>
+      </div>
+      <p class="footer">Made With ❤ ❤ ❤ By Emilson</p>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -34,18 +40,18 @@ export default {
   data() {
     return {
       lists: ["Surface Laptop 2", "Google Pixel", "Sony 1000xm4"],
-      currentList: ""
+      currentList: "",
     };
   },
   methods: {
     addList() {
-      this.listss.push(this.currentList);
+      this.lists.push(this.currentList);
       this.currentList = "";
     },
-	removeList(index){
-	this.fruits.splice(index, 1)
-	}
-  }
+    removeList(index) {
+      this.lists.splice(index, 1);
+    },
+  },
 };
 </script>
 
@@ -56,18 +62,28 @@ export default {
   justify-content: center;
 }
 
-.fa-plus{
-background-color:#FCA954;
-color:#ffffff;
-width:40px;
-height:40px;
-display:flex;
-justify-content:center;
-align-items:center;
-border-radius:30px
+.fa-plus {
+  background-color: #fca954;
+  color: #ffffff;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+}
+.icon-font{
+  font-size:40px;
+  color: #fca954;
+}
+.icon-font:hover{
+  
+  transition: all 0.3s ease-in-out;
 }
 
-.btn{color: green}
+.btn {
+  color: green;
+}
 .fruits {
   width: 300px;
   /* border: 1px solid black; */
@@ -83,25 +99,25 @@ border-radius:30px
   display: flex;
   /* flex-direction: column; */
   justify-content: space-between;
-align-items: center;
+  align-items: center;
   padding: 20px;
   margin: 10px 0px;
   background: #444444;
-  color: #AAAAAA;
+  color: #aaaaaa;
 }
 
 .container input {
   width: 80%;
   padding: 8px 0px;
-outline:none;
-background:transparent;
-border:none;
-color: #AAAAAA;
-font-size:17px;
+  outline: none;
+  background: transparent;
+  border: none;
+  color: #aaaaaa;
+  font-size: 17px;
 }
 
 ::placeholder {
-  color: #AAAAAA;
+  color: #aaaaaa;
 }
 
 .container-text {
@@ -110,12 +126,12 @@ font-size:17px;
   padding: 20px;
   margin: 20px 0px;
   background: #444444;
-  color: #AAAAAA;
+  color: #aaaaaa;
 }
-.text{
- display: flex;
+.text {
+  display: flex;
   align-items: center;
-justify-content: space-between;
+  justify-content: space-between;
 }
 
 li {
@@ -123,18 +139,23 @@ li {
   display: flex;
   justify-content: space-between;
   align-items: center;
- color: #AAAAAA;
+  color: #aaaaaa;
 }
 .footer {
-color: #AAAAAA;
-padding: 10px 0px;
+  color: #aaaaaa;
+  padding: 10px 0px;
+  text-align: center;
+}
+.icon-mouse {
+  cursor: pointer;
 }
 
-@media(max-width:332px){
-.fruits,.container-text,.container{
-width:270px;
-max-width:250px;
-
-}
+@media (max-width: 332px) {
+  .fruits,
+  .container-text,
+  .container {
+    width: 270px;
+    max-width: 250px;
+  }
 }
 </style>
